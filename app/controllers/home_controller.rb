64ed8d6447d066fa3f6a52866e23075ecc83be19
Josh Arnold <joshuaarnold22@gmail.com>
2015-11-30 14:19:38 -0500

    added instance variable for @plex

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @tasks = Task.all
-    plex = Plex.new
-    @test_array = plex.questions
+    @plex = Plex.new
+    @test_array = @plex.questions
     # @plex_data = plex.questions
   end
 end
