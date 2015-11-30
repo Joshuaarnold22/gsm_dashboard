0790a250ffca93ec519316c30f18945cb1c343bd
Josh Arnold <joshuaarnold22@gmail.com>
2015-11-30 18:54:09 -0500

    Another test

--- app/models/plex.rb
+++ app/models/plex.rb
@@ -2,19 +2,19 @@ class Plex
   include HTTParty
   base_uri 'testapi.plexonline.com'
 
-
   def initialize
     @auth = { username: ENV["PLEX_USER_NAME"], password: ENV["PLEX_PASSWORD"] }
+    test_array = []
   end
 
   def questions
-    test_array = []
+    @test_array = []
     data = self.class.get("/DataSource/Service.asmx/ExecuteDataSourceByDataSourceKey?dataSourceKey=6265", basic_auth: @auth)
-    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i
-    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][3]["Value"].to_i
-    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
-    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][18]["Value"].to_i
-    test_array
+    @test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i
+    @test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][3]["Value"].to_i
+    @test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
+    @test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][18]["Value"].to_i
+    @test_array
   end
 
   def users
