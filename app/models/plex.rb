5a61483f9d8ee5e0c78a41509371a5911a2b324b
Josh Arnold <joshuaarnold22@gmail.com>
2015-11-30 19:39:18 -0500

    testing...1...2...3...

--- app/models/plex.rb
+++ app/models/plex.rb
@@ -8,11 +8,11 @@ class Plex
 
   def questions
     test_array = []
-    data = self.class.get("http://testapi.plexonline.com/DataSource/Service.asmx/ExecuteDataSourceByDataSourceKey?dataSourceKey=6265", basic_auth: @auth)
+    data = HTTParty.get("http://testapi.plexonline.com/DataSource/Service.asmx/ExecuteDataSourceByDataSourceKey?dataSourceKey=6265", basic_auth: @auth)
     test_array.push(data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i)
     test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][3]["Value"].to_i
-    # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
-    # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][18]["Value"].to_i
+    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
+    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][18]["Value"].to_i
     test_array
   end
 
