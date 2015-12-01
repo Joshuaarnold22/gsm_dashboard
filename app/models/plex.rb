b3e3cf024ed9efdda218fdeffd9378d14ba73b47
Josh Arnold <joshuaarnold22@gmail.com>
2015-11-30 19:04:57 -0500

    testing...1...2...3...

--- app/models/plex.rb
+++ app/models/plex.rb
@@ -9,7 +9,7 @@ class Plex
   def questions
     test_array = []
     data = self.class.get("/DataSource/Service.asmx/ExecuteDataSourceByDataSourceKey?dataSourceKey=6265", basic_auth: @auth)
-    test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i
+    # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i
     # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][3]["Value"].to_i
     # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
     # test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][18]["Value"].to_i
