2dbe674dcebd16ab1d44a8733ab6f9c3f0300d12
Josh Arnold <joshuaarnold22@gmail.com>
2015-12-04 15:49:40 -0500

    loggins data from plex

--- app/models/plex.rb
+++ app/models/plex.rb
@@ -9,6 +9,8 @@ class Plex
   def questions
     test_array = []
     data = HTTParty.get("http://testapi.plexonline.com/DataSource/Service.asmx/ExecuteDataSourceByDataSourceKey?dataSourceKey=6265", basic_auth: @auth)
+    puts "here is the httparty data"
+    puts "#{data}"
     test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][0]["Value"].to_i
     test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][3]["Value"].to_i
     test_array << data["ExecuteDataSourceResult"]["ResultSets"]["ResultSet"]["Rows"]["Row"][0]["Columns"]["Column"][4]["Value"].to_i
